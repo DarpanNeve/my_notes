85c42a8f1c72bbd20902f580af8dcae7eff4e619
Darpan <darpanneve3@gmail.com>
2024-01-31 18:44:04 +0530

    initial commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -18,13 +18,36 @@ class _HomePageState extends State<HomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Form(
+              key: _formKey,
               child: Column(
                 children: [
                   Image.network(
                       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCl_3kAxtuyfdxd7RPEjOHlOGQ0UAwT5MgA&usqp=CAU'),
-                  TextFormField()
+                  TextFormField(
+                    // The validator receives the text that the user has entered.
+                    validator: (value) {
+                      if (value == null ||
+                          value.isEmpty ||
+                          !value.contains('@')) {
+                        return 'Please enter some valid email';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty || value.length < 7) {
+                        return 'Please enter some valid email';
+                      }
+                      return null;
+                    },
+                  ),
                 ],
               ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('submit'),
             ),
           ],
         ),
