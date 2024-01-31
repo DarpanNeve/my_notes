42d668fbe1ebfe9f2a9ea18cf17a3f38c7a18e4a
Darpan <darpanneve3@gmail.com>
2024-01-31 18:19:13 +0530

    initial commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Form(
-            child: Column(
-              children: [TextFormField()],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Form(
+              child: Column(
+                children: [
+                  Image.network(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCl_3kAxtuyfdxd7RPEjOHlOGQ0UAwT5MgA&usqp=CAU'),
+                  TextFormField()
+                ],
+              ),
             ),
-          ),
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
