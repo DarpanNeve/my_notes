23fbc0946651e9cdc63bf960a85d435f021dde7b
Darpan <darpanneve3@gmail.com>
2024-01-31 19:29:31 +0530

    initial commit

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -14,42 +14,61 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Image.network(
-                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCl_3kAxtuyfdxd7RPEjOHlOGQ0UAwT5MgA&usqp=CAU'),
-                  TextFormField(
-                    // The validator receives the text that the user has entered.
-                    validator: (value) {
-                      if (value == null ||
-                          value.isEmpty ||
-                          !value.contains('@')) {
-                        return 'Please enter some valid email';
-                      }
-                      return null;
-                    },
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Form(
+                  key: _formKey,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Image.network(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsCl_3kAxtuyfdxd7RPEjOHlOGQ0UAwT5MgA&usqp=CAU'),
+                        TextFormField(
+                          decoration:InputDecoration(
+                            label: Text("ID"),
+                          ),
+                          // The validator receives the text that the user has entered.
+                          validator: (value) {
+                            if (value == null ||
+                                value.isEmpty ||
+                                !value.contains('@')) {
+                              return 'Please enter some valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                        TextFormField(
+                          decoration:InputDecoration(
+                            label: Text("Password"),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty || value.length < 7) {
+                              return 'Please enter some valid email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                  TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty || value.length < 7) {
-                        return 'Please enter some valid email';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _formKey.currentState!.validate();
+                    if(!_formKey.currentState!.validate()){
+                      return ;
+                    }
+                  },
+                  child: const Text('submit'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('submit'),
-            ),
-          ],
+          ),
         ),
       ),
     );
