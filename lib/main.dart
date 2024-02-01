f9500eb66ce50771403cda60ba0a0565e610ae5c
Darpan <darpanneve3@gmail.com>
2024-02-01 18:45:27 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_notes/screens/homepage.dart';
+import 'package:my_notes/screens/notes.dart';
 
 import 'firebase_options.dart';
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const Notes(),
     );
   }
 }
